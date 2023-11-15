ea847d9f6563f00efed7d4166283360beb41bd80
matheus <89268454+mts-the-encoder@users.noreply.github.com>
2023-11-15 13:38:46 -0300

    test with selenium

---
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+using (IWebDriver driver = new EdgeDriver())
+{
+    driver.Navigate().GoToUrl("https://mts-the-encoder.github.io/Form/");
+
+    var name = driver.FindElement(By.Id("name"));
+    name.SendKeys("Rick Sanchez");
+
+    var email = driver.FindElement(By.Id("email"));
+    email.SendKeys("rick@email.com");
+
+    var message = driver.FindElement(By.Id("message"));
+    message.SendKeys("Wubba Lubba Dub Dub.");
+
+    var button = driver.FindElement(By.CssSelector("input[type='submit']"));
+    button.Click();
+
+    Console.WriteLine("DEU BOM!");
+
+    driver.Quit();
+}
+
+Console.ReadLine();
